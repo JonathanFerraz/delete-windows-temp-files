538ac5737c885d9def49a1d1abc09c29b66a9dfe
Jonathan Ferraz <jonathanferraz.chagas@gmail.com>
2021-05-25 15:44:48 -0300

    Update RemoveTemporaryFiles.bat

--- RemoveTemporaryFiles.bat
+++ RemoveTemporaryFiles.bat
@@ -1,18 +1,24 @@
-echo off
+*********START CODE************
+
+@ECHO OFF
 cls
 echo "SCRIPT Para limpar arquivos temporarios do Windows"
 pause
 echo "Limpando..."
-del /q "C:\Windows\Temp\*"
+
+del /s /f /q "C:\Windows\Temp\*"
 FOR /D %%p IN ("C:\Windows\Temp\*.*") DO rmdir "%%p" /s /q
 
-del /q "%USERPROFILE%\AppData\Local\Temp\*"
+del /s /f /q "%USERPROFILE%\AppData\Local\Temp\*"
 FOR /D %%p IN ("%USERPROFILE%\AppData\Local\Temp\*.*") DO rmdir "%%p" /s /q
 
-del /q "C:\Windows\Prefetch\*"
+del /s /f /q "C:\Windows\Prefetch\*"
 FOR /D %%p IN ("C:\Windows\Prefetch\*.*") DO rmdir "%%p" /s /q
 
-del /q "C:\Windows\SoftwareDistribution\Download\*"
+del /s /f /q "C:\Windows\SoftwareDistribution\Download\*"
 FOR /D %%p IN ("C:\Windows\SoftwareDistribution\Download\*.*") DO rmdir "%%p" /s /q
+
 echo "Limpeza concluida!"
 pause
+
+*********END CODE************
